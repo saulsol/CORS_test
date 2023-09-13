73296017e85c89073c49572180820e270640971a
saul <dlathf3210@naver.com>
2023-09-13 19:14:37 +0900

    0913

--- src/main/java/com/example/corstest/GreetingController.java
+++ src/main/java/com/example/corstest/GreetingController.java
@@ -1,9 +1,17 @@
 package com.example.corstest;
 
 
+import com.example.dto.ChartData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 @RestController
@@ -13,4 +21,23 @@ public class GreetingController {
     public String hello() {
         return "안녕하세요?";
     }
+
+
+    @GetMapping("/chart")
+    public ResponseEntity<?> chartData() throws JsonProcessingException {
+
+        List<ChartData> list = new ArrayList<>();
+        ChartData data = new ChartData("a", 10);
+        ChartData data1 = new ChartData("b", 15);
+        ChartData data2 = new ChartData("c", 18);
+        ChartData data3 = new ChartData("d", 60);
+
+        list.add(data);
+        list.add(data1);
+        list.add(data2);
+        list.add(data3);
+        return ResponseEntity.ok().body(list);
+    }
+
+
 }
---
+++ src/main/java/com/example/dto/ChartData.java
@@ -0,0 +1,15 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ChartData {
+
+    private String writer;
+    private int count;
+
+}
